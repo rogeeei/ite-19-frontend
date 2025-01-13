f5cf6f18a9e24903bb73510cbe88ba04b78f8745
unknown <cuevasrogelyn2@gmail.com>
2025-01-13 18:02:29 +0800

    updated router

--- js/router/router.js
+++ js/router/router.js
@@ -24,25 +24,18 @@ function setRouter() {
   if (
     !token &&
     (window.location.pathname === "/dashboard.html" ||
-      window.location.pathname === "/citizen.html" ||
-      window.location.pathname === "/history.html" ||
-      window.location.pathname === "/supplies.html" ||
+      window.location.pathname === "/products.html" ||
+      window.location.pathname === "/customer.html" ||
+      window.location.pathname === "/inventory.html" ||
       window.location.pathname === "/admin.html" ||
-      window.location.pathname === "/bhw.html" ||
-      window.location.pathname === "/profiling.html" ||
-      window.location.pathname === "/reports.html" ||
-      window.location.pathname === "/view_users.html" ||
-      window.location.pathname === "/demo.html" ||
-      window.location.pathname === "/service_view.html" ||
-      window.location.pathname === "/services.html" ||
-      window.location.pathname === "/stakeholder.html" ||
-      window.location.pathname === "/superadmin_report.html" ||
-      window.location.pathname === "/superadmin.html" ||
-      window.location.pathname === "/users.html" ||
-      window.location.pathname === "/admin_user_view.html" ||
-      window.location.pathname === "/user_view.html" ||
-      window.location.pathname === "/stakeholder-dashboard.html" ||
-      window.location.pathname === "/services.html")
+      window.location.pathname === "/parent_type.html" ||
+      window.location.pathname === "/product_type.html" ||
+      window.location.pathname === "/purchase.html" ||
+      window.location.pathname === "/sales.html" ||
+      window.location.pathname === "/store_sales.html" ||
+      window.location.pathname === "/store.html" ||
+      window.location.pathname === "/vendor_dashboard.html" ||
+      window.location.pathname === "/store_inventory.html")
   ) {
     window.location.pathname = "/";
     return; // Exit early to prevent further checks
